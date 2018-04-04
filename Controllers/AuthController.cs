bd1e7d088b47e633f28ce36705c3d6f4ce4ffc9d
Ivan de Assis Leal <ivanassisleal@hotmail.com>
2018-04-04 17:12:46 -0300

    Initial commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,70 @@
+﻿using AspNetJwtToken.Auth;
+using AspNetJwtToken.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Principal;
+
+namespace AspNetJwtToken.Controllers
+{
+    [Route("api/[controller]")]
+    public class AuthController : Controller
+    {
+        [AllowAnonymous]
+        [HttpGet]
+        public object Get(
+            [FromServices]SigningConfigurations signingConfigurations,
+            [FromServices]TokenConfigurations tokenConfigurations)
+        {
+            bool credenciaisValidas = true;
+
+            if (credenciaisValidas)
+            {
+                ClaimsIdentity identity = new ClaimsIdentity(
+                    new GenericIdentity("10", "Login"),
+                    new[] {
+                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                        new Claim(JwtRegisteredClaimNames.UniqueName, "10"),
+                        new Claim(ClaimTypes.Role,"ValidUsers")
+                    }
+                );
+
+                DateTime dataCriacao = DateTime.Now;
+                DateTime dataExpiracao = dataCriacao +
+                    TimeSpan.FromSeconds(tokenConfigurations.Seconds);
+
+                var handler = new JwtSecurityTokenHandler();
+                var securityToken = handler.CreateToken(new SecurityTokenDescriptor
+                {
+                    Issuer = tokenConfigurations.Issuer,
+                    Audience = tokenConfigurations.Audience,
+                    SigningCredentials = signingConfigurations.SigningCredentials,
+                    Subject = identity,
+                    NotBefore = dataCriacao,
+                    Expires = dataExpiracao
+                });
+                var token = handler.WriteToken(securityToken);
+
+                return new
+                {
+                    authenticated = true,
+                    created = dataCriacao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    expiration = dataExpiracao.ToString("yyyy-MM-dd HH:mm:ss"),
+                    accessToken = token,
+                    message = "OK"
+                };
+            }
+            else
+            {
+                return new
+                {
+                    authenticated = false,
+                    message = "Falha ao autenticar"
+                };
+            }
+        }
+    }
+}
---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetJwtToken.Controllers
+{
+
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [Authorize(Policy = "OnlyValidUsers")]
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}
